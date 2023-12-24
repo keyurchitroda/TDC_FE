4f24776b5e1cee92819d9fabf87885bfb2f208fa
keyurchitroda <keyurchitroda@gmail.com>
2023-12-24 12:56:09 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,26 +18,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Poppins:wght@700&display=swap"
-          rel="stylesheet"
-        ></link>
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
           crossOrigin="anonymous"
         ></link>
-
+      </head>
+      <body suppressHydrationWarning={true} className={inter.className}>
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
           crossOrigin="anonymous"
         ></script>
-      </head>
-      <body suppressHydrationWarning={true} className={inter.className}>
+        <Navbar />
         {children}
       </body>
     </html>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,172 +1,9 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import "./css/Navbar.css";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
-import TextField from "@mui/material/TextField";
-import { Dialog, DialogContent } from "@mui/material";
 
 export default function Navbar() {
-  const [openModal, setOpenModal] = useState(false);
-  const openSignupModal = () => {
-    setOpenModal(!openModal);
-  };
-
-  const signupModal = () => {
-    return (
-      //   <Dialog
-      //     open={openModal}
-      //     onClose={openSignupModal}
-      //     aria-labelledby="modal-modal-title"
-      //     aria-describedby="modal-modal-description"
-      //     maxWidth={300}
-      //   >
-      //     <DialogContent>
-      //       <Container component="main" maxWidth="xs">
-      //         <CssBaseline />
-      //         <div className={classes.paper}>
-      //           <Avatar className={classes.avatar}></Avatar>
-      //           <Typography component="h1" variant="h5">
-      //             Sign up
-      //           </Typography>
-      //           <form className={classes.form} noValidate>
-      //             <Grid container spacing={2}>
-      //               <Grid item xs={12} sm={6}>
-      //                 <TextField
-      //                   autoComplete="fname"
-      //                   name="firstName"
-      //                   variant="outlined"
-      //                   required
-      //                   fullWidth
-      //                   id="firstName"
-      //                   label="First Name"
-      //                   autoFocus
-      //                 />
-      //               </Grid>
-      //               <Grid item xs={12} sm={6}>
-      //                 <TextField
-      //                   variant="outlined"
-      //                   required
-      //                   fullWidth
-      //                   id="lastName"
-      //                   label="Last Name"
-      //                   name="lastName"
-      //                   autoComplete="lname"
-      //                 />
-      //               </Grid>
-      //               <Grid item xs={12}>
-      //                 <TextField
-      //                   variant="outlined"
-      //                   required
-      //                   fullWidth
-      //                   id="email"
-      //                   label="Email Address"
-      //                   name="email"
-      //                   autoComplete="email"
-      //                 />
-      //               </Grid>
-      //               <Grid item xs={12}>
-      //                 <TextField
-      //                   variant="outlined"
-      //                   required
-      //                   fullWidth
-      //                   name="password"
-      //                   label="Password"
-      //                   type="password"
-      //                   id="password"
-      //                   autoComplete="current-password"
-      //                 />
-      //               </Grid>
-      //               <Grid item xs={12}>
-      //                 <FormControlLabel
-      //                   control={
-      //                     <Checkbox value="allowExtraEmails" color="primary" />
-      //                   }
-      //                   label="I want to receive inspiration, marketing promotions and updates via email."
-      //                 />
-      //               </Grid>
-      //             </Grid>
-      //             <Button
-      //               type="submit"
-      //               fullWidth
-      //               variant="contained"
-      //               color="primary"
-      //               className={classes.submit}
-      //             >
-      //               Sign Up
-      //             </Button>
-      //             <Grid container justify="flex-end">
-      //               <Grid item>
-      //                 <Link href="#" variant="body2">
-      //                   Already have an account? Sign in
-      //                 </Link>
-      //               </Grid>
-      //             </Grid>
-      //           </form>
-      //         </div>
-      //         <Box mt={5}>
-      //           <Copyright />
-      //         </Box>
-      //       </Container>
-      //     </DialogContent>
-      //   </Dialog>
-      <>
-        <div className="view">
-          <div className="flex-center">
-            <button
-              className="btn btn-lg btn-indigo"
-              data-toggle="modal"
-              data-target="#myModal"
-            >
-              Open Modal
-            </button>
-          </div>
-        </div>
-
-        <div
-          className="modal fade"
-          id="myModal"
-          tabindex="-1"
-          role="dialog"
-          aria-labelledby="myModalLabel"
-          aria-hidden="true"
-        >
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <button
-                  type="button"
-                  className="close"
-                  data-dismiss="modal"
-                  aria-label="Close"
-                >
-                  <span aria-hidden="true">&times;</span>
-                </button>
-                <h4 className="modal-title" id="myModalLabel">
-                  Modal title
-                </h4>
-              </div>
-              <div className="modal-body">
-                <img
-                  src="https://mdbootstrap.com/images/regular/nature/img%20(25).jpg"
-                  className="img-fluid"
-                />
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-primary">
-                  Save changes
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  };
-
   return (
     <div className="container">
       <nav className="navbar navbar-expand-lg sticky-top bg-body-tertiary">
@@ -213,14 +50,9 @@ export default function Navbar() {
                 </a>
               </li>
             </ul>
-            <button
-              onClick={openSignupModal}
-              type="button"
-              className="btn btn-primary nav-btn"
-            >
+            <button type="button" className="btn btn-primary nav-btn">
               Sign up
             </button>
-            {signupModal()}
           </div>
         </div>
       </nav>
